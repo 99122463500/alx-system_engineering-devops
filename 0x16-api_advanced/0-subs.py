9be9cd82b165c1a0e79016f7d2de55c3bb6b06fe
99122463500 <karabononyane99@gmail.com>
2024-06-05 03:57:49 -0700

    0-subs.py

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,18 @@
+import requests
+
+def number_of_subscribers(subreddit):
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}  # Set a custom User-Agent to avoid errors
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data["data"]["subscribers"]
+        return subscribers
+    else:
+        return 0
+
+# Example usage
+if __name__ == '__main__':
+    subreddit = input("Enter subreddit name: ")
+    print(number_of_subscribers(subreddit))
+
